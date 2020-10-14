72722beaf43520d3d2955e98769aeb850ae3ab0f
Milton-Long <57848400+Milton-Long@users.noreply.github.com>
2020-10-14 01:23:19 -0400

    Add files via upload
    This is the R script for step 6 of milestone 1

---
+++ MathStats_Project_Milestone1_step6_script.R
@@ -0,0 +1,24 @@
+#compute mean of NA sales
+mean(VGsales$NA_Sales)
+
+#compute median of NA sales
+median(VGsales$NA_Sales)
+
+#compute variance of NA sales
+var(VGsales$NA_Sales)
+
+#compute standard deviation of NA sales
+sd(VGsales$NA_Sales)
+
+
+#compute mean of Global sales
+mean(VGsales$Global_Sales)
+
+#compute median of Global sales
+median(VGsales$Global_Sales)
+
+#compute variance of Global sales
+var(VGsales$Global_Sales)
+
+#compute standard deviation of Global sales
+sd(VGsales$Global_Sales)
